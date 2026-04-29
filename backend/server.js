dd5604325b6078233c136a41ba76b8d1b3c07612
vivekmpatne <vivekpatnem@gmail.com>
2026-04-29 08:48:47 +0530

    fix: env + deploy

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/progress", require("./routes/progress"));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
